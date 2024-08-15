b08589b53d59b4ae4a2be4c5e96c106b3248b2b2
leonard099 <leonardodamian000@gmail.com>
2024-08-15 13:56:25 -0300

    Se cambio las url de succes y failer por las de vercel

--- src/PlanDeEntranmiento/Plan.repository.ts
+++ src/PlanDeEntranmiento/Plan.repository.ts
@@ -227,8 +227,8 @@ export class PlanRepository {
           },
         ],
         back_urls: {
-          success: 'http://localhost:3000/mercadoPagoPlan/success',
-          failure: 'http://localhost:3000/mercadoPagoPlan/failure',
+          success: 'https://fit-hub-front-end.vercel.app/mercadoPagoRutina/success',
+          failure: 'https://fit-hub-front-end.vercel.app/mercadoPagoRutina/failure',
         },
         auto_return: 'approved',
        
